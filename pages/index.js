274aa872cc259d9a808e437a7b2befd1a14f1ac4
chira37 <50698591+chira37@users.noreply.github.com>
2021-05-11 15:03:27 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import ProTip from "../src/ProTip";
-import Link from "../src/Link";
-import Copyright from "../src/Copyright";
 import OrderDetails from "../componets/orderDetails/OrderDetails";
 
 
